8ae3c6139f31a9dc59df2090feac7aa5c192abe3
Alex Rosenfeld <arosenfeld2003@mac.com>
2019-04-18 18:41:45 -0700

    disabling-listeners-on-destroyed-scopes

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1903,6 +1903,16 @@ describe('Scope', function() {
       expect(listener).toHaveBeenCalled();
     });
 
+    it('no longer calls listeners after destroyed', function() {
+      var listener = jasmine.createSpy();
+      scope.$on('myEvent', listener);
+
+      scope.$destroy();
+
+      scope.$emit('myEvent');
+      expect(listener).not.toHaveBeenCalled();
+    });
+
   });
 
 });
