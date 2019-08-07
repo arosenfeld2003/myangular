207a51c8ab39fa5d147f0c4560ef3f7a49c50b4f
Alex Rosenfeld <arosenfeld2003@mac.com>
2019-08-07 07:49:59 -0700

    ensuring safety in member access

--- src/parse.js
+++ src/parse.js
@@ -324,12 +324,14 @@ ASTCompiler.prototype.compile = function(text) {
   var ast = this.astBuilder.ast(text);
   this.state = { body: [], nextId: 0, vars: [] };
   this.recurse(ast);
-  /* jshint -W054 */
-  return new Function('s', 'l',
+  var fnString = 'var fn =function(s, l){' +
     (this.state.vars.length ?
       'var ' + this.state.vars.join(',') + ';' :
       ''
-    ) + this.state.body.join(''));
+    ) + this.state.body.join('') +
+    '}; return fn;';
+  /* jshint -W054 */
+  return new Function('ensureSafeMemberName', fnString)(ensureSafeMemberName);
   /* jshint +W054 */
 };
 
@@ -356,6 +358,7 @@ ASTCompiler.prototype.recurse = function(ast, context, create) {
     }, this));
     return '{' + properties.join(',') + '}';
   case AST.Identifier:
+    ensureSafeMemberName(ast.name);
     intoId = this.nextId();
     this.if_(this.getHasOwnProperty('l', ast.name),
       this.assign(intoId, this.nonComputedMember('l', ast.name)));
@@ -383,6 +386,7 @@ ASTCompiler.prototype.recurse = function(ast, context, create) {
     }
     if (ast.computed) {
       var right = this.recurse(ast.property);
+      this.addEnsureSafeMemberName(right);
       if (create) {
         this.if_(this.not(this.computedMember(left, right)),
           this.assign(this.computedMember(left, right), '{}'));
@@ -394,6 +398,7 @@ ASTCompiler.prototype.recurse = function(ast, context, create) {
         context.computed = true;
       }
     } else {
+      ensureSafeMemberName(ast.property.name);
       if (create) {
         this.if_(this.not(this.nonComputedMember(left, ast.property.name)),
           this.assign(this.nonComputedMember(left, ast.property.name), '{}'));
@@ -483,6 +488,10 @@ ASTCompiler.prototype.nextId = function() {
   return id;
 };
 
+ASTCompiler.prototype.addEnsureSafeMemberName = function(expr) {
+  this.state.body.push('ensureSafeMemberName(' + expr + ');');
+};
+
 function Parser(lexer) {
   this.lexer = lexer;
   this.ast = new AST(this.lexer);
@@ -497,6 +506,14 @@ function parse(expr) {
   var lexer = new Lexer();
   var parser = new Parser(lexer);
   return parser.parse(expr);
+}
+
+function ensureSafeMemberName(name) {
+  if (name === 'constructor' || name === '__proto__' ||
+      name === '__defineGetter__' || name === '__defineSetter__' ||
+      name === '__lookupGetter__' || name === '__lookupSetter__') {
+    throw 'Attempting to access a disallowed field in Angular expressions!';
+  }
 }
 
 module.exports = parse;
