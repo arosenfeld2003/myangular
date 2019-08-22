167d8c003f263fc31c0006274ef613af95677dec
Alex Rosenfeld <arosenfeld2003@mac.com>
2019-08-22 09:47:12 -0700

    ensuring safe objects

--- src/parse.js
+++ src/parse.js
@@ -324,14 +324,19 @@ ASTCompiler.prototype.compile = function(text) {
   var ast = this.astBuilder.ast(text);
   this.state = { body: [], nextId: 0, vars: [] };
   this.recurse(ast);
-  var fnString = 'var fn =function(s, l){' +
+  var fnString = 'var fn=function(s, l){' +
     (this.state.vars.length ?
       'var ' + this.state.vars.join(',') + ';' :
       ''
     ) + this.state.body.join('') +
     '}; return fn;';
   /* jshint -W054 */
-  return new Function('ensureSafeMemberName', fnString)(ensureSafeMemberName);
+  return new Function(
+    'ensureSafeMemberName',
+    'ensureSafeObject',
+    fnString)(
+      ensureSafeMemberName,
+      ensureSafeObject);
   /* jshint +W054 */
 };
 
@@ -375,6 +380,7 @@ ASTCompiler.prototype.recurse = function(ast, context, create) {
       context.name = ast.name;
       context.computed = false;
     }
+    this.addEnsureSafeObject(intoId);
     return intoId;
   case AST.ThisExpression:
     return 's';
@@ -392,7 +398,8 @@ ASTCompiler.prototype.recurse = function(ast, context, create) {
           this.assign(this.computedMember(left, right), '{}'));
       }
       this.if_(left,
-        this.assign(intoId, this.computedMember(left, right)));
+        this.assign(intoId,
+          'ensureSafeObject(' + this.computedMember(left, right) + ')'));
       if (context) {
         context.name = right;
         context.computed = true;
@@ -404,7 +411,9 @@ ASTCompiler.prototype.recurse = function(ast, context, create) {
           this.assign(this.nonComputedMember(left, ast.property.name), '{}'));
       }
       this.if_(left,
-        this.assign(intoId, this.nonComputedMember(left, ast.property.name)));
+        this.assign(intoId,
+          'ensureSafeObject(' +
+          this.nonComputedMember(left, ast.property.name) + ')'));
       if (context) {
         context.name = ast.property.name;
         context.computed = false;
@@ -417,16 +426,17 @@ ASTCompiler.prototype.recurse = function(ast, context, create) {
     var callContext = {};
     var callee = this.recurse(ast.callee, callContext);
     var args = _.map(ast.arguments, _.bind(function(arg) {
-      return this.recurse(arg);
+      return 'ensureSafeObject(' + this.recurse(arg) + ')';
     }, this));
     if(callContext.name) {
+      this.addEnsureSafeObject(callContext.context);
       if (callContext.computed) {
         callee = this.computedMember(callContext.context, callContext.name);
       } else {
         callee = this.nonComputedMember(callContext.context, callContext.name);
       }
     }
-    return callee + '&&' + callee + '(' + args.join(',') + ')';
+    return callee + '&&ensureSafeObject(' + callee + '(' + args.join(',') + '))';
   case AST.AssignmentExpression:
     var leftContext = {};
     this.recurse(ast.left, leftContext, true);
@@ -436,7 +446,8 @@ ASTCompiler.prototype.recurse = function(ast, context, create) {
     } else {
       leftExpr = this.nonComputedMember(leftContext.context, leftContext.name);
     }
-    return this.assign(leftExpr, this.recurse(ast.right));
+    return this.assign(leftExpr,
+      'ensureSafeObject(' + this.recurse(ast.right) + ')');
   }
 };
 
@@ -492,6 +503,10 @@ ASTCompiler.prototype.addEnsureSafeMemberName = function(expr) {
   this.state.body.push('ensureSafeMemberName(' + expr + ');');
 };
 
+ASTCompiler.prototype.addEnsureSafeObject = function(expr) {
+  this.state.body.push('ensureSafeObject(' + expr + ');');
+};
+
 function Parser(lexer) {
   this.lexer = lexer;
   this.ast = new AST(this.lexer);
@@ -514,6 +529,23 @@ function ensureSafeMemberName(name) {
       name === '__lookupGetter__' || name === '__lookupSetter__') {
     throw 'Attempting to access a disallowed field in Angular expressions!';
   }
+}
+
+function ensureSafeObject(obj) {
+  if(obj) {
+    if(obj.window === obj) {
+      throw 'Referencing window in Angular expressions is disallowed';
+    } else if (obj.children &&
+      // Q: what is obj.prop, obj.attr or obj.find?  How would this be triggered??
+                (obj.nodeName || (obj.prop && obj.attr && obj.find))) {
+      throw 'Referencing DOM nodes in Angular expressions is disallowed!';
+    } else if (obj.constructor === obj) {
+      throw 'Referencing Function in Angular expressions is disallowed!';
+    } else if (obj === Object) {
+      throw 'Referencing Function in Angular expressions is disallowed!';
+    }
+  }
+  return obj;
 }
 
 module.exports = parse;
